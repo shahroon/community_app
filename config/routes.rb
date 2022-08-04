851fbb9095fe170881fe9ea84f823b81bf6cfc05
Shahroon Ali <shahroon.ali@gmail.com>
2022-08-04 15:59:36 +0500

    devise integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Re-directs signing out user back to sign in
+    get "users", to: "devise/sessions#new"
+  end
+
+  get "welcome/index"
+  
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
