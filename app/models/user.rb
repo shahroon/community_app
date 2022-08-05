c357afe79cb7ec36732817072fde8329ae917e85
Shahroon Ali <shahroon.ali@gmail.com>
2022-08-05 18:46:32 +0500

    @wip for DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+  has_many :memberships
+  has_many :groups, through: :memberships
 end
